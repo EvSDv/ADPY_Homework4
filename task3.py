c58d6fdc80f4ad25de8f1d9f1028d967236620ac
Vyacheslav Eykhe <vyacheslav.eyhe@gmail.com>
2019-01-30 15:09:05 +0900

    Готово

--- task3.py
+++ task3.py
@@ -1,27 +1,85 @@
 from utils.decorator import decorator_maker
 
 
-@decorator_maker('log.txt')
-def adv_print(*args, start='', max_line=0, in_file=False):
-    result = str(start)
-    result_sep = ''
-    for value in args:
-        result += str(value)
 
-    if max_line > 0 and len(result) > max_line:
-        for symbol in range(0, len(result), max_line):
-            result_sep += result[symbol:symbol + max_line] + '\n'
-        result = result_sep
+class Contact:
 
-    if in_file:
-        with open('result.txt', 'w', encoding='utf8') as file:
-            file.write(result)
-        return result
-    else:
-        return result
+    def __init__(self, name, surname, phone, favorite=False, *args, **kwargs):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.favorite = favorite
+        self.other_list = []
+        self.other_dic = {}
+
+        for value in args:
+            self.other_list.append(value)
+
+        for key, value in kwargs.items():
+            self.other_dic[key] = value
+
+    def __str__(self):
+
+        other_list_string = ''
+        other_dic_string = ''
+
+        def return_word():
+            if self.favorite:
+                return 'да'
+            else:
+                return 'нет'
+
+        main_string = f' Имя: {self.name} \n Фамилия: {self.surname} \n Телефон: {self.phone} \n ' \
+            f'В избранных: {return_word()} \n Дополнительная информация: \n '
+
+        for item in self.other_list:
+            other_list_string += f'\t \t {item} \n'
+
+        for key in self.other_dic.keys():
+            other_dic_string += f'\t \t {key} : {self.other_dic[key]} \n'
+
+        return main_string + other_list_string + other_dic_string
+
+
+class PhoneBook:
+    phonebook = []
+
+    def __init__(self, name):
+        self.book_name = name
+
+# Декоратор записывает лог запуска метода в файл
+    @decorator_maker('log.txt')
+    def add_contact(self, name, surname, phone, *args, **kwargs):
+        new_contact = Contact(name, surname, phone,  *args, **kwargs)
+        self.phonebook.append(new_contact)
+        return new_contact
+
+    def show_contacts(self):
+        for contact in self.phonebook:
+            print(contact)
+
+    def del_contact(self, phone):
+        for position, contact in enumerate(self.phonebook):
+            if phone in str(contact):
+                self.phonebook.pop(position)
+
+    def search_favorite_contact(self):
+        for contact in self.phonebook:
+            if contact.favorite:
+                print(contact)
+
+    def search_contact_by_name(self, name, surname):
+        for contact in self.phonebook:
+            if name and surname in str(contact):
+                print(contact)
 
 
 if __name__ == '__main__':
+    a = PhoneBook('First phonebook')
 
-    adv_print(31153, '1111111111', start='*', max_line=5, in_file=True)
-    adv_print('Строка для примера', start='*', max_line=5, in_file=False)
+    a.add_contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
+    a.add_contact('Allford', 'Jenkin', '+2123482626', telegram='@jenkin', email='jenkin@smith.com')
+    a.add_contact('Audley', 'Kingsman', '+2029398907', True, '+12025912173', telegram='@kingsman',
+                  email='kingsman@smith.com', test='Kingsman Audley')
+
+    
--- utils/decorator.py
+++ utils/decorator.py
@@ -10,7 +10,7 @@ def decorator_maker(path):
                 log_string += f'{now.strftime("%d-%m-%Y %H:%M:%S")} | Имя функции: {function_to_decorate.__name__} | ' \
                           f'Переданные аргументы: {args} {kwargs} | Результат: \n'
                 result_string = function_to_decorate(*args, **kwargs)
-                log_string += result_string + '\n'
+                log_string += str(result_string) + '\n'
                 log.write(log_string)
         return cover_function
 
